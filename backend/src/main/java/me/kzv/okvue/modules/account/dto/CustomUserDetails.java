b27e1e718d61419d552adc63292cbe3386ef4008
kimzerovirus <yg9699k@naver.com>
2022-08-08 23:55:40 +0900

    customuserdetails 추가

--- backend/src/main/java/me/kzv/okvue/modules/account/dto/CustomUserDetails.java
+++ backend/src/main/java/me/kzv/okvue/modules/account/dto/CustomUserDetails.java
@@ -1,58 +1,82 @@
 package me.kzv.okvue.modules.account.dto;
 
 import lombok.Getter;
+import me.kzv.okvue.modules.account.Account;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 
 @Getter
 public class CustomUserDetails implements OAuth2User, UserDetails {
 
+    private Account account;
+    private Map<String, Object> attributes;
+
+    // 일반 시큐리티 로그인시 사용
+    public CustomUserDetails(Account user) {
+        this.account = user;
+    }
+
+    // OAuth2.0 로그인시 사용
+    public CustomUserDetails(Account account, Map<String, Object> attributes) {
+        this.account = account;
+        this.attributes = attributes;
+    }
+
+    public Account getUser() {
+        return account;
+    }
+
     @Override
     public String getPassword() {
-        return null;
+        return account.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return account.getEmail();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
-    }
-
-    @Override
-    public Map<String, Object> getAttributes() {
-        return null;
+        return true;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        Collection<GrantedAuthority> collet = new ArrayList<GrantedAuthority>();
+        collet.add(()->{ return account.getAuthority();});
+        return collet;
     }
 
+    // 리소스 서버로 부터 받는 회원정보
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    // Account 의 PK
     @Override
     public String getName() {
-        return null;
+        return account.getId().toString();
     }
 }
