d96b72c68b49222014e0684662b64acd2d9496a0
kimzerovirus <yg9699k@naver.com>
2023-01-09 13:38:36 +0900

    app 설정

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head />
-			<body>
+			<body className="flex h-full select-none flex-col font-sans text-gray-900 antialiased dark:bg-gray-800 dark:text-gray-100 sm:select-auto">
 				<Main />
 				<NextScript />
 			</body>
