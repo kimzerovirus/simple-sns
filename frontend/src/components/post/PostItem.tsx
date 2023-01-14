6f861ca47855d52f0f36f5481fc10a54ae6b77a6
kimzerovirus <yg9699k@naver.com>
2023-01-14 23:35:29 +0900

    태그 레이아웃 수정

--- frontend/src/components/post/PostItem.tsx
+++ frontend/src/components/post/PostItem.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import { TagList } from 'src/components/post/Tags';
 
@@ -33,29 +34,38 @@ const PostItem = () => {
 						</a>
 					</div>
 					<div className="flex">
-						<TagList
-							tagList={['태그1', '태그2', '태그3', '태그3', '태그3', '태그3', '태그3', '태그3']}
-							size="xs"
-						/>
-						<div className="w-full flex justify-end items-center gap-x-2 text-gray-700 dark:text-gray-300">
+						<div className="flex items-center gap-x-1 sm:gap-x-2 w-full">
+							{['태그1', '태그2', '태그3', '태그3', '태그3', '태그3', '태그3', '태그3'].map(
+								(tagName, i) => (
+									<Link
+										key={i}
+										className="line-clamp-1 text-xs font-normal leading-5 text-gray-600 dark:text-gray-400 sm:text-sm"
+										href="/"
+									>
+										#{tagName}
+									</Link>
+								),
+							)}
+						</div>
+						<div className="flex justify-end items-center gap-x-2 text-gray-700 dark:text-gray-300">
 							<div className="flex items-center space-x-0.5 text-xs sm:text-sm">
 								<svg
 									xmlns="http://www.w3.org/2000/svg"
 									fill="none"
 									viewBox="0 0 24 24"
-									stroke-width="1.5"
+									strokeWidth="1.5"
 									stroke="currentColor"
 									aria-hidden="true"
 									className="h-4 w-4"
 								>
 									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
+										strokeLinecap="round"
+										strokeLinejoin="round"
 										d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
 									></path>
 									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
+										strokeLinecap="round"
+										strokeLinejoin="round"
 										d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
 									></path>
 								</svg>
--- frontend/src/components/post/PostView.tsx
+++ frontend/src/components/post/PostView.tsx
@@ -22,8 +22,24 @@ interface PostViewProps {
 const PostView: React.FC<PostViewProps> = ({ content, title, tags }) => {
 	return (
 		<Container>
+			<div className="relative mt-8 mb-4">
+				<div className="absolute inset-0 flex items-center">
+					<div className="w-full border-t border-gray-500/30 dark:border-gray-500/70"></div>
+				</div>
+				<div className="relative ml-2 flex font-normal sm:ml-5">
+					<div className="bg-white px-2 text-gray-500 dark:bg-gray-800 dark:text-gray-400">
+						<Link
+							className="text-sm text-gray-400 hover:text-primary-light dark:hover:text-primary-dark"
+							href="/community"
+						>
+							커뮤니티
+						</Link>
+					</div>
+				</div>
+			</div>
+
 			{/* TITLE */}
-			<h1 className="block break-all text-xl font-semibold leading-7 sm:text-3xl sm:leading-10 mt-7">
+			<h1 className="block break-all text-xl font-semibold leading-7 sm:text-3xl sm:leading-10">
 				공부중인 학생인데 제가 너무 바보같아요....
 			</h1>
 			<div className="w-full flex justify-between text-base font-normal">
@@ -48,19 +64,19 @@ const PostView: React.FC<PostViewProps> = ({ content, title, tags }) => {
 							xmlns="http://www.w3.org/2000/svg"
 							fill="none"
 							viewBox="0 0 24 24"
-							stroke-width="1.5"
+							strokeWidth="1.5"
 							stroke="currentColor"
 							aria-hidden="true"
 							className="h-4 w-4"
 						>
 							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
+								strokeLinecap="round"
+								strokeLinejoin="round"
 								d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
 							></path>
 							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
+								strokeLinecap="round"
+								strokeLinejoin="round"
 								d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
 							></path>
 						</svg>
@@ -81,21 +97,7 @@ const PostView: React.FC<PostViewProps> = ({ content, title, tags }) => {
 				</div>
 			</div>
 
-			<div className="relative mt-2 mb-7">
-				<div className="absolute inset-0 flex items-center">
-					<div className="w-full border-t border-gray-500/30 dark:border-gray-500/70"></div>
-				</div>
-				<div className="relative ml-2 flex font-normal sm:ml-5">
-					<div className="bg-white px-2 text-gray-500 dark:bg-gray-800 dark:text-gray-400">
-						<Link
-							className="text-sm text-gray-400 hover:text-primary-light dark:hover:text-primary-dark"
-							href="/community"
-						>
-							커뮤니티
-						</Link>
-					</div>
-				</div>
-			</div>
+			<Border className="mt-6 mb-8" />
 
 			{/* CONTENT - VIEWER */}
 			<Viewer content={content} />
@@ -105,17 +107,17 @@ const PostView: React.FC<PostViewProps> = ({ content, title, tags }) => {
 				<div className="flex space-x-2">
 					<button
 						type="button"
-						className="relative flex w-[100px] py-4 items-center justify-center rounded-md border border-gray-500/30 bg-white text-gray-700 hover:border-gray-500/70 focus:z-10 focus:outline-none focus:ring-0 dark:border-gray-500/70 dark:bg-gray-700 dark:text-gray-300 dark:hover:border-gray-500/30"
+						className="relative flex w-[120px] md:w-[136px] py-2 text-xs md:text-sm items-center justify-center rounded-md border border-gray-500/30 bg-white text-gray-700 hover:border-gray-500/70 focus:z-10 focus:outline-none focus:ring-0 dark:border-gray-500/70 dark:bg-gray-700 dark:text-gray-300 dark:hover:border-gray-500/30"
 					>
 						<i className="bx bx-like mr-1"></i>
 						<span>추천 5</span>
 					</button>
 					<button
 						type="button"
-						className="relative flex py-4 w-[100px] items-center justify-center rounded-md border border-gray-500/30 bg-white text-gray-700 hover:border-gray-500/70 focus:z-10 focus:outline-none focus:ring-0 dark:border-gray-500/70 dark:bg-gray-700 dark:text-gray-300 dark:hover:border-gray-500/30"
+						className="relative flex w-[120px] md:w-[136px] py-2 text-xs md:text-sm items-center justify-center rounded-md border border-gray-500/30 bg-white text-gray-700 hover:border-gray-500/70 focus:z-10 focus:outline-none focus:ring-0 dark:border-gray-500/70 dark:bg-gray-700 dark:text-gray-300 dark:hover:border-gray-500/30"
 					>
 						<i className="bx bx-dislike mr-1"></i>
-						<span>비추천 3</span>
+						<span>비추천 -3</span>
 					</button>
 				</div>
 			</div>
@@ -123,7 +125,7 @@ const PostView: React.FC<PostViewProps> = ({ content, title, tags }) => {
 			<Border className="mt-8 mb-4" />
 
 			{/* TAGS */}
-			<TagList tagList={['태그1', '태그2', '태그3']} size="sm" />
+			<TagList tagList={['태그1', '태그2', '태그3']} size="xs" />
 
 			{/* REPLY */}
 		</Container>
